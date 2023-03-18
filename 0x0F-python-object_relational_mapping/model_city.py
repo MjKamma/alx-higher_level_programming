5a86a72411b8844438b8341e461460f1babd3708
MjKamma <Mercyigwe1995@gmail.com>
2023-03-18 16:48:13 -0700

    tasks on Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""Definition of the City class"""
-
-from sqlalchemy.ext.declarative import declarative_base
+""" Contains the class definition of a City
+"""
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
-    """Class City"""
-
+    """
+    Class that defines each city
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
