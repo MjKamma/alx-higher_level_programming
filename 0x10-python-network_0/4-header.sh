252eae08fca72e35c257d2548fc2b8d2f15c635c
MjKamma <Mercyigwe1995@gmail.com>
2023-03-30 21:48:42 -0700

    python network task

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# send a GET request to the URL using curl, and display the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
